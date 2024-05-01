9f4fd8e747abba3927155add509265d57b93fbeb
MUSTAFA TÜREDİ <116067203+MUSTAFA221809100@users.noreply.github.com>
2024-05-02 02:42:04 +0300

    İNTERNET PROGRAMCILIĞI 2 PROJELERİM
    Product_Category_Model / Branches_Model

---
+++ Uygulama-3/Uygulama-3/application/views/branches_v/list/content.php
@@ -0,0 +1,75 @@
+    <!-- Content Wrapper. Contains page content -->
+    <div class="content-wrapper">
+      <!-- Content Header (Page header) -->
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <div class="row">
+            <div class="col-12">
+              <div class="card">
+                <div class="card-header">
+                  <div class="row">
+
+                    <div class="col-md-6">
+                      <h3 class="card-title">Şubeler Listesi</h3>
+                    </div>
+
+                    <div class="col-md-6 text-right">
+                      <a href="<?php echo base_url("Branches/new_form") ?>" class="btn btn-success btn-xs mb-2 ">
+                      <i class="fas fa-plus"></i> Yeni Şube Ekle</a>
+                    </div>
+
+                  </div>
+
+                </div>
+                <!-- /.card-header -->
+                <div class="card-body">
+                  <table id="example1" class="table table-bordered table-striped">
+                    <thead>
+                      <tr>
+                        <th>ID</th>
+                        <th>Şube Adı</th>
+                        <th>Adres</th>
+                        <th>Oluşturma Tarihi</th>
+                        <th>İşlemler</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+
+                      <?php foreach ($items as $item) {  ?>
+                        <tr>
+                          <td><?php echo $item->id; ?></td>
+                          <td><?php echo $item->title; ?></td>
+                          <td><?php echo $item->address ?></td>
+                          <td><?php echo dateTimeFormat($item->createdAt); ?></td>
+                          <td>
+                          <a href="<?php echo base_url("Branches/delete/$item->id")?>" class="btn btn-danger" > SİL </a>  
+                          
+                          <a href="<?php echo base_url("Branches/update_form/$item->id")?>" class="btn btn-info" > GÜNCELLE </a></td>
+                        </tr>
+                      <?php } ?>
+
+                    </tbody>
+                  </table>
+                </div>
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </div>
+            <!-- /.col -->
+          </div>
+          <!-- /.row -->
+        </div>
+        <!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
+
+    </div>
+    <!-- /.content-wrapper -->
